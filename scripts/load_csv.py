ca85ce7c5af6081c801897678af2f449dfc2084f
weisserw <will.a.weisser@gmail.com>
2025-01-13 19:59:38 -0500

    don't import invalid matches

--- scripts/load_csv.py
+++ scripts/load_csv.py
@@ -168,6 +168,11 @@ def process_file(csv_file_path, no_scores):
                             ):
                                 continue
 
+                            red_winner = row["Red Winner"].lower() == "true"
+                            blue_winner = row["Blue Winner"].lower() == "true"
+                            if red_winner and blue_winner:
+                                continue # match hasn't finished, don't import
+
                             event = get_or_create_event_or_athlete(
                                 db.session,
                                 Event,
@@ -226,8 +231,6 @@ def process_file(csv_file_path, no_scores):
                                     row["Red Note"], row["Blue Note"]
                                 ),
                             )
-                            red_winner = row["Red Winner"].lower() == "true"
-                            blue_winner = row["Blue Winner"].lower() == "true"
 
                             match = Match(
                                 happened_at=datetime.strptime(
