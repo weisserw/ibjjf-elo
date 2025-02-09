855e56217e15683c779e2df2ca0a2bb45712fe10
weisserw <will.a.weisser@gmail.com>
2025-02-09 12:07:50 -0500

    modify query

--- queries/open_matches.sql
+++ queries/open_matches.sql
@@ -1,6 +1,8 @@
 select d.gi, d.age, d.belt, d.gender, red_a.name as name1, blue_a.name as name2,
-       round(red.start_rating) as rating1, round(blue.start_rating) as rating2,
-       red.weight_for_open as weight1, blue.weight_for_open as weight2,
+       round(red.start_rating) as start_rating1, round(red.end_rating) as end_rating1,
+       round(blue.start_rating) as start_rating2, round(blue.end_rating) as end_rating2,
+       d.weight as weight_division,
+       red.weight_for_open as athlete_weight1, blue.weight_for_open as athlete_weight2,
        red.winner as winner1, blue.winner as winner2
 from matches m
 join divisions d on d.id = m.division_id
@@ -20,6 +22,6 @@ cross join lateral (
     limit 1
 ) blue
 join athletes blue_a on blue_a.id = blue.athlete_id
-where d.weight like 'Open%' and red.weight_for_open is not null and blue.weight_for_open is not null and m.rated
+where red.weight_for_open is not null and blue.weight_for_open is not null and m.rated
 order by m.happened_at
 ;
