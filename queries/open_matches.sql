5f596ddac4f0dfab23c735706b564546ab84435c
weisserw <will.a.weisser@gmail.com>
2025-02-10 13:25:36 -0500

    add happened_at and rated_winner_only columns and allow non-open matches

--- queries/open_matches.sql
+++ queries/open_matches.sql
@@ -1,9 +1,9 @@
-select d.gi, d.age, d.belt, d.gender, red_a.name as name1, blue_a.name as name2,
+select m.happened_at, d.gi, d.age, d.belt, d.gender, red_a.name as name1, blue_a.name as name2,
        round(red.start_rating) as start_rating1, round(red.end_rating) as end_rating1,
        round(blue.start_rating) as start_rating2, round(blue.end_rating) as end_rating2,
        d.weight as weight_division,
        red.weight_for_open as athlete_weight1, blue.weight_for_open as athlete_weight2,
-       red.winner as winner1, blue.winner as winner2
+       red.winner as winner1, blue.winner as winner2, m.rated_winner_only
 from matches m
 join divisions d on d.id = m.division_id
 cross join lateral (
@@ -22,6 +22,6 @@ cross join lateral (
     limit 1
 ) blue
 join athletes blue_a on blue_a.id = blue.athlete_id
-where red.weight_for_open is not null and blue.weight_for_open is not null and m.rated
+where m.rated
 order by m.happened_at
 ;
