ea9313ee8d90e05e68823ff0f5996a62667a4bc9
weisserw <will.a.weisser@gmail.com>
2025-02-22 16:47:54 -0500

    record deleted matches to s3

--- scripts/delete_match.py
+++ scripts/delete_match.py
@@ -3,6 +3,10 @@
 import sys
 import os
 import uuid
+import csv
+import json
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "app"))
 
@@ -10,6 +14,48 @@ import argparse
 from app import db, app
 from models import Match, MatchParticipant
 from ratings import recompute_all_ratings
+
+
+def get_s3_client():
+    aws_creds = json.loads(os.getenv("AWS_CREDS"))
+    return boto3.client(
+        "s3",
+        aws_access_key_id=aws_creds["aws_access_key_id"],
+        aws_secret_access_key=aws_creds["aws_secret_access_key"],
+        region_name=aws_creds.get("region"),
+    )
+
+
+def download_deleted_matches_file(s3_client, bucket_name, file_name):
+    file_path = os.path.join(os.getcwd(), file_name)
+    try:
+        s3_client.download_file(bucket_name, file_name, file_path)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            print(f"{file_name} not found in bucket. A new file will be created.")
+        else:
+            print(f"An error occurred: {e}")
+            raise
+    except (NoCredentialsError, PartialCredentialsError) as e:
+        print(f"Credentials error: {e}")
+        raise
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise
+    return file_path
+
+
+def upload_deleted_matches_file(s3_client, file_path, bucket_name, file_name):
+    try:
+        s3_client.upload_file(file_path, bucket_name, file_name)
+        print(f"{file_path}: File uploaded to S3.")
+    except (NoCredentialsError, PartialCredentialsError) as e:
+        print(f"Credentials error: {e}")
+        raise
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Delete a match and update ratings")
@@ -26,6 +72,16 @@ if __name__ == "__main__":
         print(f"Invalid match ID: {args.match_id}")
         sys.exit(1)
 
+    s3_client = get_s3_client()
+    bucket_name = os.getenv("S3_BUCKET")
+    if not bucket_name:
+        raise ValueError("S3_BUCKET environment variable not set")
+
+    deleted_matches_file = "deleted_matches.csv"
+    file_path = download_deleted_matches_file(
+        s3_client, bucket_name, deleted_matches_file
+    )
+
     with app.app_context():
         match = db.session.query(Match).filter_by(id=match_uuid).first()
 
@@ -36,6 +92,9 @@ if __name__ == "__main__":
         division = match.division
         gi = division.gi
         gender = division.gender
+        age = division.age
+        weight = division.weight
+        belt = division.belt
         happened_at = match.happened_at
 
         participants = (
@@ -45,9 +104,12 @@ if __name__ == "__main__":
             print(f"No participants found for match ID {args.match_id}.")
             sys.exit(1)
 
+        athlete_1_name = match.participants[0].athlete.name
+        athlete_2_name = match.participants[1].athlete.name
+
         print(
             f"Deleting match that occurred on {match.happened_at.isoformat()} between "
-            f"{match.participants[0].athlete.name} and {match.participants[1].athlete.name}, "
+            f"{athlete_1_name} and {athlete_2_name}, "
             f"gi = {division.gi}, gender = {division.gender}."
         )
 
@@ -66,6 +128,24 @@ if __name__ == "__main__":
                 rerank=False,
                 athlete_id=athlete_id,
             )
+
+        with open(file_path, "a", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                [
+                    match.event.ibjjf_id,
+                    match.event.name,
+                    athlete_1_name,
+                    athlete_2_name,
+                    "Gi" if gi else "No-Gi",
+                    f"{age} / {gender} / {belt} / {weight}",
+                    happened_at.isoformat(),
+                ]
+            )
+
+        upload_deleted_matches_file(
+            s3_client, file_path, bucket_name, deleted_matches_file
+        )
 
         db.session.commit()
 
