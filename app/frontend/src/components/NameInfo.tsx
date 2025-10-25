bf6ac3fd9c0d9e4706fbed2151f311f3e4d8dcd7
weisserw <will.a.weisser@gmail.com>
2025-10-24 21:04:24 -0400

    make tooltips work on mobile and fix bugs where tooltips dont close

--- app/frontend/src/components/NameInfo.tsx
+++ app/frontend/src/components/NameInfo.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import igLogo from '/src/assets/instagram.png';
 import igLogoColor from '/src/assets/instagram-color.png';
 import { getCountryName } from '../utils';
 import { useAppContext } from '../AppContext';
-import { Tooltip } from 'react-tooltip';
+import { Tooltip, type TooltipRefProps } from 'react-tooltip';
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 import "./NameInfo.css";
@@ -36,6 +36,7 @@ const competitorMedal = (medal: string | null | undefined) => {
 
 function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_image_url, country, country_note, country_note_pt, medal, tree }: NameInfoProps) {
   const [uniqueId] = useState(() => Math.random().toString(36).substring(2, 9));
+  const igTooltipRef = useRef<TooltipRefProps>(null);
 
   const {
     language,
@@ -47,21 +48,34 @@ function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_
   return (
     <div className="name-subinfo">
       <Tooltip id={uniqueId} className="tooltip-normal" />
-      <Tooltip id={`${uniqueId}-ig`} className="tooltip-ig" clickable place="top">
-        <div className="ig-tooltip-content">
-          <a href={`https://www.instagram.com/${instagram_profile}`} target="_blank" rel="noopener noreferrer" className="ig-tooltip-username">
-            <img src={profile_image_url ?? ''} alt={`@${instagram_profile}`} className="ig-tooltip-photo" />
-            <div className="ig-tooltip-name">
-              {instagram_profile_personal_name ?? `@${instagram_profile}`} <img src={igLogoColor} alt="Instagram" className="ig-tooltip-instagram-logo" />
-            </div>
-          </a>
-        </div>
-      </Tooltip>
       {country && (
         <span className={`fi fi-${country.trim().toLowerCase().substring(0, 2)} country-flag`} data-tooltip-place="top" data-tooltip-id={uniqueId} data-tooltip-content={getCountryName(country, country_note, country_note_pt, language)} />
       )}
-      {instagram_profile && (
-        <a className={tree ? "instagram-profile-tree" : "instagram-profile"} href={`https://www.instagram.com/${instagram_profile}`} target="_blank" rel="noopener noreferrer" data-tooltip-id={profile_image_url ? `${uniqueId}-ig` : undefined}>
+      {
+        instagram_profile && profile_image_url &&
+          <Tooltip id={`${uniqueId}-ig`} className="tooltip-ig" clickable place="top" ref={igTooltipRef}>
+            <div className="ig-tooltip-content" onMouseLeave={
+              () => {
+                igTooltipRef.current?.close();
+              }
+            }>
+              <a href={`https://www.instagram.com/${instagram_profile}`} target="_blank" rel="noopener noreferrer" className="ig-tooltip-username">
+                <img src={profile_image_url ?? ''} alt={`@${instagram_profile}`} className="ig-tooltip-photo" />
+                <div className="ig-tooltip-name">
+                  {instagram_profile_personal_name ?? `@${instagram_profile}`} <img src={igLogoColor} alt="Instagram" className="ig-tooltip-instagram-logo" />
+                </div>
+              </a>
+            </div>
+          </Tooltip>
+      }
+      {
+        instagram_profile && profile_image_url &&
+        <span className={tree ? "instagram-profile-tree" : "instagram-profile"} data-tooltip-id={`${uniqueId}-ig`}>
+          <img src={igLogo} alt="Instagram" title={`@${instagram_profile}`} />
+        </span>
+      }
+      {instagram_profile && !profile_image_url && (
+        <a className={tree ? "instagram-profile-tree" : "instagram-profile"} href={`https://www.instagram.com/${instagram_profile}`} target="_blank" rel="noopener noreferrer">
           <img src={igLogo} alt="Instagram" title={`@${instagram_profile}`} />
         </a>
       )}
