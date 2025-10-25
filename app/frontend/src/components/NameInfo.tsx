2967d8aa534ebe77094aa974ef6c74f76fcf1f5a
weisserw <will.a.weisser@gmail.com>
2025-10-24 22:22:47 -0400

    try to fix tooltip problems

--- app/frontend/src/components/NameInfo.tsx
+++ app/frontend/src/components/NameInfo.tsx
@@ -1,9 +1,9 @@
-import { useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import igLogo from '/src/assets/instagram.png';
 import igLogoColor from '/src/assets/instagram-color.png';
 import { getCountryName } from '../utils';
 import { useAppContext } from '../AppContext';
-import { Tooltip, type TooltipRefProps } from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 import "./NameInfo.css";
@@ -36,7 +36,10 @@ const competitorMedal = (medal: string | null | undefined) => {
 
 function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_image_url, country, country_note, country_note_pt, medal, tree }: NameInfoProps) {
   const [uniqueId] = useState(() => Math.random().toString(36).substring(2, 9));
-  const igTooltipRef = useRef<TooltipRefProps>(null);
+
+  // manage hover state for instagram tooltip because of react-tooltip bug that doesn't close it sometimes
+  const [linkHover, setLinkHover] = useState(false);
+  const [tooltipHover, setTooltipHover] = useState(false);
 
   const {
     language,
@@ -45,6 +48,34 @@ function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_
   if (!instagram_profile && !country && !medal) {
     return null;
   }
+
+
+  useEffect(() => {
+    // clicking anywhere other than the tooltip should close it
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!tooltipHover && !linkHover) return;
+
+      const target = event.target as HTMLElement
+      if (!target.isConnected) {
+        return
+      }
+
+      const span = document.querySelector<HTMLElement>(`[id='${uniqueId}-ig-span']`);
+      const tooltip = document.querySelector<HTMLElement>(`[id='${uniqueId}-ig']`);
+      const anchors = [span, tooltip]
+      if (anchors.some((anchor) => anchor?.contains(target))) {
+        return
+      }
+
+      setTooltipHover(false);
+      setLinkHover(false);
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="name-subinfo">
       <Tooltip id={uniqueId} className="tooltip-normal" />
@@ -52,13 +83,9 @@ function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_
         <span className={`fi fi-${country.trim().toLowerCase().substring(0, 2)} country-flag`} data-tooltip-place="top" data-tooltip-id={uniqueId} data-tooltip-content={getCountryName(country, country_note, country_note_pt, language)} />
       )}
       {
-        instagram_profile && profile_image_url &&
-          <Tooltip id={`${uniqueId}-ig`} className="tooltip-ig" clickable place="top" ref={igTooltipRef}>
-            <div className="ig-tooltip-content" onMouseLeave={
-              () => {
-                igTooltipRef.current?.close();
-              }
-            }>
+        instagram_profile && profile_image_url && (tooltipHover || linkHover) &&
+          <Tooltip id={`${uniqueId}-ig`} className="tooltip-ig" clickable place="top" isOpen={true}>
+            <div className="ig-tooltip-content" onMouseEnter={() => setTooltipHover(true)} onMouseLeave={() => setTooltipHover(false)}>
               <a href={`https://www.instagram.com/${instagram_profile}`} target="_blank" rel="noopener noreferrer" className="ig-tooltip-username">
                 <img src={profile_image_url ?? ''} alt={`@${instagram_profile}`} className="ig-tooltip-photo" />
                 <div className="ig-tooltip-name">
@@ -70,7 +97,8 @@ function NameInfo({ instagram_profile, instagram_profile_personal_name, profile_
       }
       {
         instagram_profile && profile_image_url &&
-        <span className={tree ? "instagram-profile-tree" : "instagram-profile"} data-tooltip-id={`${uniqueId}-ig`}>
+        <span id={`${uniqueId}-ig-span`} className={tree ? "instagram-profile-tree" : "instagram-profile"} data-tooltip-id={`${uniqueId}-ig`}
+              onMouseEnter={() => setLinkHover(true)} onMouseDown={() => setLinkHover(true)} onMouseLeave={() => setTimeout(() => setLinkHover(false), 200)}>
           <img src={igLogo} alt="Instagram" title={`@${instagram_profile}`} />
         </span>
       }
